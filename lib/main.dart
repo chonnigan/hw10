d5af0a0a3c27da85d3efef267d5551953572d89b
chonnigan <khamwang_c@silpakorn.edu>
2022-02-26 15:01:55 +0700

    homework_10

---
+++ lib/main.dart
@@ -0,0 +1,177 @@
+import 'dart:math';
+import 'package:flutter/cupertino.dart';
+
+import 'insta.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  const app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // callback method
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cloudy',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      ),
+      home: Test(),
+    );
+  }
+}
+
+class Test extends StatelessWidget {
+  Test({Key? key}) : super(key: key);
+
+  final List<Insta> InstaList = [
+    Insta(imguser: 'assets/images/homer-simpson.jpeg', img: 'assets/images/Why-sunglasses-are-still-important-on-a-cloudy-day-672x372-1-1200x720.jpg',
+      userpost: 'ezgg', comments: [Comments(user: 'cherry_', comment: 'I love this pic'),],
+      caption: 'Have A Good Day \n#niceday',),
+    Insta(imguser: 'assets/images/lisa-simpson-fba59060-3c95-4538-8767-0f8acaa05ef-resize-750.jpeg', img: 'assets/images/sunshine-on-a-cloudy-day-cara-moulds.jpg',
+        userpost: 'muffins',comments: [Comments(user: 'story_tg', comment: '👍👍👍👍'),Comments(user: 'zapp', comment: 'nice'),Comments(user: 'harvy', comment: 'awwwwwwww')],
+        caption: 'goodvibe    #chilling_day'),
+    Insta(imguser: 'assets/images/98b371cd43ead599d22bb40590df8287.jpg', img: 'assets/images/cloudy-day-1.jpg',
+        userpost: 'story_tg',comments: [Comments(user: 'muffins', comment: 'take care of you naka')],
+        caption: '#beautiful_place \n#cloudy_sky'),
+  ];
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(title: Text('Cloudy',
+          style: TextStyle(fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.normal,
+            fontSize: 25.0,))),
+      body: ListView.builder(
+          itemCount: InstaList.length,
+          itemBuilder: (context, index) =>
+              MyCard(insta: InstaList[index])),
+
+    );
+  }
+}
+class MyCard extends StatefulWidget {
+  final Insta insta;
+
+  const MyCard({
+    Key? key,
+    required this.insta,
+  }) : super(key: key);
+
+  @override
+  _MyCardState createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+  final controller = TextEditingController();
+  var like = false;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5.0,
+      margin: EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //Text(covid.date),
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 23,
+                  backgroundImage: AssetImage('${widget.insta.imguser}'),
+                ),
+                SizedBox(width: 10.0),
+                Expanded(
+                    child: Text(
+                        '${widget.insta.userpost}'
+                    )
+                ),
+                IconButton(onPressed: (){}, icon: Icon(Icons.more_vert),)
+              ],
+            ),
+          ),
+          Image.asset('${widget.insta.img}',fit: BoxFit.cover,),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                /*SizedBox(width: 5.0),*/
+                Expanded(
+                    child: Text(
+                        '${widget.insta.caption}'))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0,left: 16.0,right: 16.0),
+            child: Column(
+              children: [
+                for(int i=0;i<widget.insta.comments.length;i++)
+                  Row(
+                    children: [
+                      SizedBox(height: 23.0,),
+                      Text(
+                        '${widget.insta.comments[i].user}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                      SizedBox(width: 5.0,),
+                      Text(
+                        '${widget.insta.comments[i].comment}',
+                        style: TextStyle(
+
+                        ),
+                      )
+                    ],
+                  ),
+              ],
+            ),
+          ),
+
+
+          Row(
+            children: [
+              IconButton(onPressed: (){
+                setState(() {
+                  like = !like;
+                });
+              }, icon: like? Icon(Icons.favorite,color: Colors.pink,): Icon(Icons.favorite_border),),
+              Expanded(
+                child: TextField(
+                  onSubmitted: (value){
+
+                    setState(() {
+                      widget.insta.comments.add(Comments(user: 'Flutter', comment: value));
+                      controller.clear();
+
+                    });
+                  },
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    hintText: 'Add a comment',
+                  ),
+                ),
+              ),
+            ],
+          )
+
+
+        ],
+      ),
+    );
+  }
+}
